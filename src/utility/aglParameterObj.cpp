ad3323421f5246d82009a3b58be70fabece0285b
AboodXD <aboood40091@gmail.com>
2022-11-26 14:09:07 +0400

    utility/IParameterObj: Implement pushBackListNode()

--- src/utility/aglParameterObj.cpp
+++ src/utility/aglParameterObj.cpp
@@ -1,3 +1,4 @@
+#include <codec/seadHashCRC32.h>
 #include <utility/aglParameter.h>
 #include <utility/aglParameterObj.h>
 
@@ -9,6 +10,19 @@ IParameterObj::IParameterObj()
     mChildParameter.initOffset(offsetof(ParameterBase, mListNode));
 
     mName = sead::SafeString::cEmptyString;
+}
+
+void IParameterObj::pushBackListNode(ParameterBase* p_node)
+{
+    u32 child_hash[1024];
+    u32 child_num = 0;
+
+    for (sead::OffsetList<ParameterBase>::iterator itr = mChildParameter.begin(), itr_end = mChildParameter.end(); itr != itr_end; ++itr)
+        child_hash[child_num++] = itr->mHash;
+
+    mChildHash = sead::HashCRC32::calcHash(child_hash, child_num * sizeof(u32));
+
+    mChildParameter.pushBack(p_node);
 }
 
 } }
