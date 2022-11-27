44644b179596799053da7829a4280c92842d90fd
AboodXD <aboood40091@gmail.com>
2022-11-27 10:28:36 +0400

    utility/DebugTexturePage: Implement ctor and cleanUp()

--- include/utility/aglDebugTexturePage.h
+++ include/utility/aglDebugTexturePage.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <container/seadBuffer.h>
+#include <container/seadTList.h>
 #include <heap/seadDisposer.h>
+#include <prim/seadSafeString.h>
 
 namespace agl { namespace utl {
 
-class DebugTexturePage : public sead::IDisposer
+class DebugTexturePage : public sead::IDisposer, public sead::TListNode<DebugTexturePage*>
 {
+public:
+    class Context;
+
 public:
     DebugTexturePage();
 
@@ -17,7 +23,16 @@ public:
     void cleanUp();
 
 private:
-    u32 _10[0x1CC / sizeof(u32)];
+    sead::FixedSafeString<128> _20;
+    u8 _ac;
+    u32 mCurrentContext;
+    sead::Buffer<Context> mContext;
+    sead::FixedSafeString<128> _bc;
+    sead::FixedSafeString<128> _148;
+    u8 _1d4;
+    u8 _1d5;
+    u8 _1d6;
+    u32 _1d8;
 };
 static_assert(sizeof(DebugTexturePage) == 0x1DC, "agl::utl::DebugTexturePage size mismatch");
 
