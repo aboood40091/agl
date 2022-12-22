9fe30297c63d52c0a299b8924f68ded144b186c4
AboodXD <aboood40091@gmail.com>
2022-12-22 17:16:18 +0400

    utility/TextureMemoryAllocator: Implement free()
    It was NOT my pleasure to decompile this function.

---
+++ src/utility/aglTextureMemoryAllocator.cpp
@@ -0,0 +1,73 @@
+#include <utility/aglTextureMemoryAllocator.h>
+
+namespace agl { namespace utl {
+
+void TextureMemoryAllocator::free(MemoryBlock* p_memory)
+{
+    // SEAD_ASSERT(p_memory != nullptr);
+
+    mUsedSize -= p_memory->mBufferSize;
+
+    if (p_memory->mpBufferFromDebugHeap)
+    {
+        delete[] p_memory->mpBufferFromDebugHeap;
+        p_memory->mpBufferFromDebugHeap = NULL;
+    }
+
+    mMemoryBlockUsed.erase(p_memory);
+
+    if (!mMemoryBlockFree.front())
+    {
+        mMemoryBlockFree.pushFront(p_memory);
+        return;
+    }
+
+    for (sead::OffsetList<MemoryBlock>::iterator itr = mMemoryBlockFree.begin(), itr_end = mMemoryBlockFree.end(); itr != itr_end; ++itr)
+    {
+        MemoryBlock* p_temp_memory = &(*itr);
+
+        if (p_memory->mpBuffer + p_memory->mBufferSize <= p_temp_memory->mpBuffer)
+        {
+            mMemoryBlockFree.insertBefore(p_temp_memory, p_memory);
+
+            if (p_memory->mpBuffer + p_memory->mBufferSize == p_temp_memory->mpBuffer)
+            {
+                p_memory->mBufferSize += p_temp_memory->mBufferSize;
+                mMemoryBlockFree.erase(p_temp_memory);
+                mMemoryBlockEmpty.pushBack(p_temp_memory);
+            }
+
+            MemoryBlock* p_prev = mMemoryBlockFree.prev(p_memory);
+            if (p_prev && p_prev->mpBuffer + p_prev->mBufferSize == p_memory->mpBuffer)
+            {
+                p_memory->mpBuffer = p_prev->mpBuffer;
+                p_memory->mBufferSize += p_prev->mBufferSize;
+                mMemoryBlockFree.erase(p_prev);
+                mMemoryBlockEmpty.pushBack(p_prev);
+            }
+
+            break;
+        }
+    }
+
+    if (!p_memory->mListNode.isLinked())
+    {
+        mMemoryBlockFree.pushBack(p_memory);
+
+        MemoryBlock* p_prev = mMemoryBlockFree.prev(p_memory);
+        if (p_prev && p_prev->mpBuffer + p_prev->mBufferSize == p_memory->mpBuffer)
+        {
+            p_memory->mpBuffer = p_prev->mpBuffer;
+            p_memory->mBufferSize += p_prev->mBufferSize;
+            mMemoryBlockFree.erase(p_prev);
+            mMemoryBlockEmpty.pushBack(p_prev);
+        }
+    }
+
+    for (sead::OffsetList<MemoryBlock>::iterator itr = mMemoryBlockFree.begin(), itr_end = mMemoryBlockFree.end(); itr != itr_end; ++itr)
+    {
+        // Ok...
+    }
+}
+
+} }
