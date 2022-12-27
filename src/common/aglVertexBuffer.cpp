b6274ef34ead051734a1a111318a74a6159bdddd
AboodXD <aboood40091@gmail.com>
2022-12-27 23:51:08 +0400

    common/VertexBuffer: Implement setUpStream()

--- src/common/aglVertexBuffer.cpp
+++ src/common/aglVertexBuffer.cpp
@@ -53,4 +53,29 @@ void VertexBuffer::setUpBuffer(const void* buffer, u32 stride, u32 buffer_byte_s
     DCFlushRangeNoSync(mpBuffer, mBufferByteSize);
 }
 
+void VertexBuffer::setUpStream(s32 index, VertexStreamFormat format, u32 offset)
+{
+    mStream[index].mFormat = format;
+    mStream[index].mOffset = offset;
+    mStream[index].mInitialized = true;
+
+    const GX2CompSel comp_sel[] = {
+        GX2_COMP_SEL_X001, GX2_COMP_SEL_XY01,
+        GX2_COMP_SEL_X001, GX2_COMP_SEL_X001,
+        GX2_COMP_SEL_XY01, GX2_COMP_SEL_X001,
+        GX2_COMP_SEL_X001, GX2_COMP_SEL_XY01,
+        GX2_COMP_SEL_XY01, GX2_COMP_SEL_XYZ1,
+        GX2_COMP_SEL_XYZW, GX2_COMP_SEL_XYZW,
+        GX2_COMP_SEL_XY01, GX2_COMP_SEL_XY01,
+        GX2_COMP_SEL_XYZW, GX2_COMP_SEL_XYZW,
+        GX2_COMP_SEL_XYZ1, GX2_COMP_SEL_XYZ1,
+        GX2_COMP_SEL_XYZW, GX2_COMP_SEL_XYZW
+    };
+
+    mStream[index].mCompSel = comp_sel[format & 0xff];
+    mStream[index].mDivisor = 0;
+    mStream[index].mEndianSwap = GX2_ENDIANSWAP_DEFAULT;
+    mStream[index].mIndexType = GX2_ATTRIB_INDEX_VERTEX_ID;
+}
+
 }
